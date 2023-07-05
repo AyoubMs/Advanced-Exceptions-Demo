48bf8df7262d04c8b553db6483db52f5761a7c80
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:12:39 +0200

    add app

---
+++ ConsoleUI/ConsoleUIMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class ConsoleUIMethods
+    {
+        internal static void SimpleMethod()
+        {
+            throw new InvalidOperationException("You should not be calling the SimpleMethod");
+        }
+
+        internal static void DifferentMethod()
+        {
+            Console.WriteLine("This is the different method working properly.");
+        }
+
+        internal static void ComplexMethod(string name)
+        {
+            if (name.ToLower() == "tim")
+            {
+                //throw new InsufficientMemoryException("Tim is too tall for this method.");
+                //throw new OutOfMemoryException("This is an out of memory exception");
+                throw new SystemException("I am a system exception");
+            }
+            else
+            {
+                throw new ArgumentException("This person isn't Tim.");
+            }
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+
+using ConsoleUI;
+string name = "";
+try
+{
+    ConsoleUIMethods.DifferentMethod();
+    Console.Write("What is your name: ");
+    name = Console.ReadLine();
+    ConsoleUIMethods.ComplexMethod(name);
+    ConsoleUIMethods.SimpleMethod();
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine("You should not be calling these methods.");
+    Console.WriteLine(ex.Message);
+}
+catch (NotImplementedException)
+{
+    Console.WriteLine("You forgot to finish your code!!!");
+}
+catch (Exception ex) when (name.ToLower() == "tim")
+{
+    Console.WriteLine("You used Tim's name, didn't you?");
+    Console.WriteLine(ex.Message);
+    //Environment.Exit(1);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    //Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("I always run");
+}
+
+Console.ReadLine();
